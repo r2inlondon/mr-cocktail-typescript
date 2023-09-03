bc56fc19e9c68efd426c8ff4899cbc29463893e8
Arturo Quiroz <r2inlondon@gmail.com>
2023-09-03 19:37:14 +0100

    feat : installed prettier-plugin-tailwindcss. Also added some logo.svg to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,22 @@
-import { Fragment } from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
 
 const NavBar = () => (
-  <Fragment>
-    <ul className="flex justify-between bg-black text-white">
+  <div className="bg-black">
+    <ul className="flex h-16 items-center justify-between text-white">
+      <li>
+        <img src={logo} alt="Logo" className="h-10 text-white invert " />
+      </li>
       <li>
         <Link to="/">
           <p className="">Home</p>
         </Link>
       </li>
-      <li>
+      {/* <li>
         <Link to="/new">New Cocktail</Link>
-      </li>
+      </li> */}
     </ul>
-  </Fragment>
+  </div>
 );
 
 export default NavBar;
